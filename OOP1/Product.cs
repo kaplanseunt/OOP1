122eed4be379b884ef90a6b17b88d65b0e444c04
Taha Kaplan <66365263+kaplanseunt@users.noreply.github.com>
2021-12-08 14:12:15 +0300

    Proje dosyası ekle.

---
+++ OOP1/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+    class Product
+    {
+
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        public string ProductName { get; set; }
+        public double UnitPrice { get; set; }
+        public int UnitsInStock { get; set; }
+
+
+
+       //Creat-Read-Update-Delete CRUD Operations
+       //Class 2'ye ayrılır. Birinde sadece özellikler olur.Birinde operasyonlar/metodlar olur.
+    }
+}
---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+    class ProductManager
+    {
+        public void Add(Product product)
+        {
+            Console.WriteLine(product.ProductName +  "eklendi");
+        }
+        public int Topla(int sayi1, int sayi2)
+        {
+            return sayi1 + sayi2;
+        }
+                                        //ikisi arasındaki fark, bir değeri başka yerde de kullanmak istedigimizde
+                                        //1. metodu kullanırız. 2. yöntemde sadece sonucu yazar bize.
+        public void Topla2(int sayi1, int sayi2)
+        {
+            Console.WriteLine(sayi1 + sayi2);
+        }
+
+
+    }
+}
